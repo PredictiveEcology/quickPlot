e0900d2b31987d58d37741fab20200511e2d2e02
Alex Chubaty <alex.chubaty@gmail.com>
2017-08-02 12:37:27 -0600

    suppress outputs from examples in tests

--- tests/testthat/test-examples.R
+++ tests/testthat/test-examples.R
@@ -1,6 +1,7 @@
 test_that("all exported functions have examples", {
   sapply(ls("package:quickPlot"), function(x) {
-    expect_warning(example(x, package = "quickPlot", character.only = TRUE), NA)
+    expect_warning(example(x, package = "quickPlot", character.only = TRUE,
+                           echo = FALSE), NA)
   })
 })
 
