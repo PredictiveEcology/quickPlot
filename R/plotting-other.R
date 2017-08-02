0478d90ac429ce9228b57b2f33772d670d93d691
Alex Chubaty <alex.chubaty@gmail.com>
2017-08-02 12:42:33 -0600

    don't run click examples

--- R/plotting-other.R
+++ R/plotting-other.R
@@ -156,7 +156,28 @@ setMethod("clearPlot",
 #' @include plotting-classes.R
 #' @rdname quickPlotMouseClicks
 #'
-#' @example inst/examples/example_click.R
+#' @examples
+#' \dontrun{
+#'   library(igraph)
+#'   library(raster)
+#'   
+#'   files <- system.file("maps", package = "quickPlot") %>%
+#'     dir(., full.names = TRUE, pattern = "tif")
+#'   maps <- lapply(files, function(x) raster(x))
+#'   names(maps) <- sapply(basename(files), function(x) {
+#'     strsplit(x, split = "\\.")[[1]][1]
+#'   })
+#'   landscape <- stack(maps$DEM, maps$forestCover, maps$habitatQuality)
+#'   
+#'   clearPlot()
+#'   Plot(landscape)
+#'   clickValues(3) # click at three locations on the Plot device
+#'   
+#'   clearPlot()
+#'   Plot(landscape)
+#'   e <- clickExtent() # click at two locations on the Plot device
+#'   print(e)
+#' }
 #'
 clickValues <- function(n = 1) {
   coords <- clickCoordinates(n = n)
