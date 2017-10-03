53bc42e8a9af84bfeaabf38106dc01579496e2ca
Alex Chubaty <alex.chubaty@gmail.com>
2017-10-03 10:19:09 -0600

    fix failing test + redoc

--- tests/testthat/test-Plot-nonVisual.R
+++ tests/testthat/test-Plot-nonVisual.R
@@ -1,67 +1,75 @@
-test_that("Plotting types without visual checking works",{
+test_that("Plotting types without visual checking works", {
+  library(sp)
+  library(raster)
+  
+  on.exit({
+    detach("package:raster")
+    detach("package:sp")
+  }, add = TRUE)
+  
   r1 <- raster(xmn = 0, xmx = 10, ymn = 0, ymx = 10,
-                      vals = sample(1:4, replace = TRUE, size = 100), res = 1)
+               vals = sample(1:4, replace = TRUE, size = 100), res = 1)
   
   r2 <- raster(xmn = 0, xmx = 10, ymn = 0, ymx = 10,
                vals = sample(1:4, replace = TRUE, size = 100), res = 1)
   
   s1 <- stack(r1,r2)
   sp1 <- SpatialPoints(cbind(x = stats::runif(10, -50, 50),
-                                         y = stats::runif(10, -50, 50)))
+                             y = stats::runif(10, -50, 50)))
   
-  if(interactive()) {
+  if (interactive()) {
     clearPlot()
     expect_silent(Plot(t1)) #agentMatrix
     a <- .getQuickPlot(paste0("quickPlot", dev.cur()))
-    expect_true(length(a$isBaseLayer)==1)
-    expect_true(length(a$curr@quickPlotGrobList)==1)
+    expect_true(length(a$isBaseLayer) == 1)
+    expect_true(length(a$curr@quickPlotGrobList) == 1)
     
     expect_silent(Plot(sp1))
     a <- .getQuickPlot(paste0("quickPlot", dev.cur()))
-    expect_true(length(a$isBaseLayer)==2)
-    expect_true(length(a$curr@quickPlotGrobList)==2)
+    expect_true(length(a$isBaseLayer) == 2)
+    expect_true(length(a$curr@quickPlotGrobList) == 2)
     
-    expect_silent(Plot(w1, axes = TRUE, new=TRUE))
+    expect_silent(Plot(w1, axes = TRUE, new = TRUE))
     a <- .getQuickPlot(paste0("quickPlot", dev.cur()))
-    expect_true(length(a$isBaseLayer)==3)
-    expect_true(length(a$curr@quickPlotGrobList)==3)
-    expect_true(length(a$curr@quickPlotGrobList$w1)==1)
+    expect_true(length(a$isBaseLayer) == 3)
+    expect_true(length(a$curr@quickPlotGrobList) == 3)
+    expect_true(length(a$curr@quickPlotGrobList$w1) == 1)
     
     expect_silent(Plot(t1, addTo = "w1"))
     a <- .getQuickPlot(paste0("quickPlot", dev.cur()))
-    expect_true(length(a$isBaseLayer)==3)
-    expect_true(length(a$curr@quickPlotGrobList)==3)
-    expect_true(length(a$curr@quickPlotGrobList$w1)==2)
+    expect_true(length(a$isBaseLayer) == 3)
+    expect_true(length(a$curr@quickPlotGrobList) == 3)
+    expect_true(length(a$curr@quickPlotGrobList$w1) == 2)
     
     expect_silent(Plot(w2))
     a <- .getQuickPlot(paste0("quickPlot", dev.cur()))
-    expect_true(length(a$isBaseLayer)==4)
-    expect_true(length(a$curr@quickPlotGrobList)==4)
-    expect_true(length(a$curr@quickPlotGrobList$w2)==1)
+    expect_true(length(a$isBaseLayer) == 4)
+    expect_true(length(a$curr@quickPlotGrobList) == 4)
+    expect_true(length(a$curr@quickPlotGrobList$w2) == 1)
     
     expect_silent(Plot(a1))
     a <- .getQuickPlot(paste0("quickPlot", dev.cur()))
-    expect_true(length(a$isBaseLayer)==6)
-    expect_true(length(a$curr@quickPlotGrobList)==6)
-    expect_true(length(a$curr@quickPlotGrobList$`a1$w1`)==1)
+    expect_true(length(a$isBaseLayer) == 6)
+    expect_true(length(a$curr@quickPlotGrobList) == 6)
+    expect_true(length(a$curr@quickPlotGrobList$`a1$w1`) == 1)
     
     expect_silent(Plot(t1, addTo = "a1$w1"))
     a <- .getQuickPlot(paste0("quickPlot", dev.cur()))
-    expect_true(length(a$isBaseLayer)==6)
-    expect_true(length(a$curr@quickPlotGrobList)==6)
-    expect_true(length(a$curr@quickPlotGrobList$`a1$w1`)==2)
+    expect_true(length(a$isBaseLayer) == 6)
+    expect_true(length(a$curr@quickPlotGrobList) == 6)
+    expect_true(length(a$curr@quickPlotGrobList$`a1$w1`) == 2)
     
     expect_silent(Plot(s1))
     a <- .getQuickPlot(paste0("quickPlot", dev.cur()))
-    expect_true(length(a$isBaseLayer)==8)
-    expect_true(length(a$curr@quickPlotGrobList)==8)
-    expect_true(length(a$curr@quickPlotGrobList$`a1$w1`)==2)
+    expect_true(length(a$isBaseLayer) == 8)
+    expect_true(length(a$curr@quickPlotGrobList) == 8)
+    expect_true(length(a$curr@quickPlotGrobList$`a1$w1`) == 2)
     
     expect_silent(Plot(r1))
     a <- .getQuickPlot(paste0("quickPlot", dev.cur()))
-    expect_true(length(a$isBaseLayer)==9)
-    expect_true(length(a$curr@quickPlotGrobList)==9)
-    expect_true(length(a$curr@quickPlotGrobList$`a1$w1`)==2)
+    expect_true(length(a$isBaseLayer) == 9)
+    expect_true(length(a$curr@quickPlotGrobList) == 9)
+    expect_true(length(a$curr@quickPlotGrobList$`a1$w1`) == 2)
     
     s1$layer.1[3] <- 15
     s1$layer.2[3] <- 25
@@ -74,8 +82,7 @@ test_that("Plotting types without visual checking works",{
     #Plot(a1, t1)
     expect_silent(rePlot())
     a <- .getQuickPlot(paste0("quickPlot", dev.cur()))
-    expect_true(length(a$isBaseLayer)==9)
-    expect_true(length(a$curr@quickPlotGrobList)==9)
-    
+    expect_true(length(a$isBaseLayer) == 9)
+    expect_true(length(a$curr@quickPlotGrobList) == 9)
   }
 })
