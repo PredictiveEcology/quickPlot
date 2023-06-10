7d3ececda52367e6e647215b5a3762fefd7d5a27
Eliot McIntire <eliotmcintire@gmail.com>
2023-06-09 22:10:18 -0400

    R CMD check on Rhub

--- revdep/check.R
+++ revdep/check.R
@@ -4,7 +4,7 @@ library("revdepcheck")
 options(repos = c(CRAN = 'https://cloud.r-project.org'))
 
 revdep_reset()
-revdep_check(num_workers = getOption("Ncpus", 1))
+revdep_check(num_workers = getOption("Ncpus", 2))
 revdep_report_cran() ## update cran-comments with this output
 
 ### email maintainers of revdep packages (need to edit: `revdep/email.yml`)
