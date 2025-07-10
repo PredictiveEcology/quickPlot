d54aec432908f932e0006b02e4090f2b2600fce3
Alex Chubaty <achubaty@for-cast.ca>
2025-07-09 22:11:19 -0600

    check PE pkgs with revdeps

--- revdep/check.R
+++ revdep/check.R
@@ -1,12 +1,18 @@
 # pak::pak("r-lib/revdepcheck")
 library("revdepcheck")
 
-options(repos = c(CRAN = 'https://cloud.r-project.org'))
+options(repos = c(
+  PE = "https://predictiveecology.r-universe.dev",
+
+  ## note this is counter to the "Canonical CRAN.r-project.org" for CRAN packages,
+  ## but revdep/ directory is .Rbuildignored and this is used for revdep checks only.
+  CRAN = paste0("https://", "cloud.", "r-project.", "org")
+))
 
 revdep_reset()
-revdep_check(num_workers = getOption("Ncpus", 2))
+revdep_check(num_workers = getOption("Ncpus", 2), quiet = FALSE, timeout = 3600) ## 60 mins
 revdep_report_cran() ## update cran-comments with this output
 
 ### email maintainers of revdep packages (need to edit: `revdep/email.yml`)
-#revdep_email(type = "broken") ## will send via gmail
-#revdep_email(type = "failed") ## will send via gmail
+# revdep_email(type = "broken") ## will send via gmail
+# revdep_email(type = "failed") ## will send via gmail
